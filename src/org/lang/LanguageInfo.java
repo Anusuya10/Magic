6ceea4c77b7496c86cb8396295a5cc60eb7f76bb
Anusuya10 <anubbytaj2510@gmail.com>
2022-05-21 20:56:10 +0530

    bbb

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,27 @@
+package org.lang;
+
+public class LanguageInfo extends StateDetails{
+	private void tamilLanguage() {
+		System.out.println("Mahabharat");
+
+	}
+	private void englishLanguage() {
+	System.out.println("Butterfly");
+
+	}
+	private void hindiLanguage() {
+		System.out.println("Zindagi");
+
+	}
+	public static void main(String[] args) {
+	     LanguageInfo a = new LanguageInfo();
+	     a.tamilLanguage();
+	     a.englishLanguage();
+	     a.hindiLanguage();
+	     a.southIndia();
+	     a.northIndia();
+
+
+	}
+
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,14 @@
+package org.lang;
+
+public class StateDetails {
+	public void southIndia() {
+		System.out.println("TamilNadu");
+
+	}
+	public void northIndia() {
+		System.out.println("Mumbai");
+
+	}
+	
+
+}
